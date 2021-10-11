0b076a0d933c89d9cf04b0a4e9c128e294183572
freakingrocky <rakshan793@gmail.com>
2021-10-11 22:04:46 +0800

    Hero Section (In Progress)

---
+++ diversmushroom/src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import './Hero.css';
+
+function Hero() {
+    return (
+        <div className="hero-container">
+            <video src='/videos/vid.mp4' autoPlay loop muted />
+            <h1>DIVERSMUSHROOM</h1>
+            <p><center>Mushrooms are more interesting than you probably think. <br />
+            Let's talk mushrooms!</center></p>
+        </div>
+)}
+
+export default Hero;
