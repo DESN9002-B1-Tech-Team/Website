60fb97667ca9b8130f6dd1c1e1d3fbc5c074e8a9
freakingrocky <rakshan793@gmail.com>
2021-10-11 21:31:38 +0800

    NavBar Completed

---
+++ diversmushroom/src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    return (
+        <>
+        <nav className="navbar">
+            <div className="navbar-container">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                    LOGO
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu disabled'}>
+                    <li className='nav-item'>
+                        <Link to="/" className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to="/posts" className='nav-links' onClick={closeMobileMenu}>
+                            Posts
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to="/tutorial" className='nav-links' onClick={closeMobileMenu}>
+                            Tutorial
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to="/buy" className='nav-links' onClick={closeMobileMenu}>
+                            Buy The Kit
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        </>
+    );
+}
+
+export default Navbar
