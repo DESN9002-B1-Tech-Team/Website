cf32b71b71fbab829a4b73f1159eb0effaa92ca2
Rocky <rakshan793@gmail.com>
2021-10-14 14:49:20 +0800

    Added Base Code for ContactUs Section

---
+++ diversmushroom/src/components/ContactUs.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './ContactUs.css'
+
+function ContactUs() {
+    return (
+    <div className='contact'>
+        <h1>Contact Us</h1>
+        <div className='contact__form'>
+            {/* Code the form here */}
+        </div>
+    </div>
+)}
+
+export default ContactUs;
--- diversmushroom/src/components/pages/home.js
+++ diversmushroom/src/components/pages/home.js
@@ -1,12 +1,14 @@
 import '../../App.css'
 import Hero from '../Hero'
 import Cards from '../Cards'
+import ContactUs from '../ContactUs'
 
 function Home() {
     return (
         <>
             <Hero />
             <Cards />
+            <ContactUs />
         </>
     )
 }
