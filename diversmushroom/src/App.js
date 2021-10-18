daa3a90d799de4578c4b71fcb8c4ac259062f364
Teresa <92213628+trslys00@users.noreply.github.com>
2021-10-18 21:40:00 +0800

    Added page navigation

--- diversmushroom/src/App.js
+++ diversmushroom/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import React from 'react';
 import Footer from './components/Footer'
 import Navbar from './components/Navbar';
-import Home from './components/pages/home'
+import Home from './components/pages/home';
+import Posts from './components/pages/posts';
 import NotFound from './components/pages/404'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/posts" exact component={Posts} />
         <Route component={NotFound} />
       </Switch>
       <Footer />
