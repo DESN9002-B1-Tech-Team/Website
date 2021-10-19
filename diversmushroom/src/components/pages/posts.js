63851ba4a919e1471d7fb137e2aca876d06ba6b5
Teresa <92213628+trslys00@users.noreply.github.com>
2021-10-19 23:00:30 +0800

    Update posts.js

--- diversmushroom/src/components/pages/posts.js
+++ diversmushroom/src/components/pages/posts.js
@@ -1,1 +1,16 @@
+import '../../App.css'
+import Hero from '../Hero'
+import Cards from '../Cards'
+import ContactUs from '../ContactUs'
 
+function Posts() {
+    return (
+        <>
+            <Hero />
+            <Cards />
+            <ContactUs />
+        </>
+    )
+}
+
+export default Posts;
