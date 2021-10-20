2d5ed4df624c50cf19baa5c6bba5a763ff52df54
Teresa <92213628+trslys00@users.noreply.github.com>
2021-10-20 12:51:52 +0800

    Added Posts and css file

--- diversmushroom/src/components/Posts.js
+++ diversmushroom/src/components/Posts.js
@@ -2,13 +2,13 @@ import '../../App.css'
 import Hero from '../Hero'
 import Cards from '../Cards'
 import ContactUs from '../ContactUs'
+import './Posts.css';
+
 
 function Posts() {
     return (
         <>
-            <Hero />
-            <Cards />
-            <ContactUs />
+            <h1> hello world </h1>
         </>
     )
 }
--- diversmushroom/src/components/pages/home.js
+++ diversmushroom/src/components/pages/home.js
@@ -8,6 +8,7 @@ function Home() {
         <>
             <Hero />
             <Cards />
+            <Posts />
             <ContactUs />
         </>
     )
