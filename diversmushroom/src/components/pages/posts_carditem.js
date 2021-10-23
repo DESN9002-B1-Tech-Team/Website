e72155638e7536dc82ef444ded0e56ad2aa632a8
Teresa <92213628+trslys00@users.noreply.github.com>
2021-10-23 23:07:03 +0800

    Linked to articles

--- diversmushroom/src/components/pages/posts_carditem.js
+++ diversmushroom/src/components/pages/posts_carditem.js
@@ -5,14 +5,14 @@ function PostCardItem(props) {
     return (
         <>
             	
-            	<div className="post_item" href="{props.path}">
+            	<div className="post_item"><a href={props.path}>
             		<div className="post_label" style={{backgroundColor:"green"}}>{props.label}</div>
             		<img src={props.src} alt="Article Image" className="post_img" />
             		<div className="post_text">
             			<h3>{props.text}</h3>
             			<p>{props.subtext}</p>
             		</div>
-            	</div>
+            	</a></div>
         </>
 )}
 
--- diversmushroom/src/data/postDetails.js
+++ diversmushroom/src/data/postDetails.js
@@ -7,13 +7,6 @@ const postDetails = [
     subtext: "The 5 edible mushrooms that are grown are Oyster mushrooms, Shiitake mushrooms, Wine cap mushrooms, Pioppino mushrooms and Lion's mane mushrooms...",
   },
   {
-    src: "/images/mushroom-in-the-dark.jpg",
-    text: "Benefits of eating mushroom",
-    label: "Interesting Facts",
-    path: "/posts/mushrooms-spore-reuse",
-    subtext: "According to a research paper Mushroom intake and depression... Mushrooms contain numerous bioactive compounds that may be associated with reduced anxiety including vitamin... ",
-  },
-  {
     src: "/images/mushroom-packaging.jpg",
     text: "Did you know mushrooms can be used for packaging?",
     label: "Interesting Facts",
@@ -40,6 +33,13 @@ const postDetails = [
     label: "Tutorial",
     path: "/posts/mushrooms-spore-reuse",
     subtext: "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. ",
+  },
+{
+    src: "/images/mushroom-in-the-dark.jpg",
+    text: "Benefits of eating mushroom",
+    label: "Interesting Facts",
+    path: "/posts/mushrooms-spore-reuse",
+    subtext: "According to a research paper Mushroom intake and depression... Mushrooms contain numerous bioactive compounds that may be associated with reduced anxiety including vitamin... ",
   },
 
  
